8dc45cada03b739d09848e59295180b001b0bd8b
carlos <c.pradenas02@ufromail.cl>
2023-09-02 22:42:57 -0400

    dia mas caluroso y frio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,49 +2,44 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] matriz = imprimirMatrizCompletada();
-        calcularSumaDeFilas(matriz);
-        calcularPromedioDeFilas(matriz);
+        iniciar();
     }
 
-    public static int[][] imprimirMatrizCompletada(){
+    public static void iniciar() {
+        int[][] semana = imprimirMatrizCompletada();
+        double[] promedios = calcularPromedioDeFilas(semana);
+        double diaMasCaluroso = obtenerDiaMasCaluroso(promedios);
+        double diaMasFrio = obtenerDiaMasFrio(promedios);
+        System.out.println("el promedio más bajo es: " + diaMasFrio);
+    }
+
+    public static int[][] imprimirMatrizCompletada() {
         int[][] semana = crearMatrizSemana();
         llenarMatrizSemana(semana);
         mostrarMatrizSemana(semana);
         return semana;
     }
-    public static int [][] crearMatrizSemana(){
+
+    public static int[][] crearMatrizSemana() {
         return new int[7][24];
     }
-    public static void mostrarMatrizSemana(int [][] semana){
-        for (int i = 0; i < semana.length; i++){
-            for (int j = 0; j < semana[0].length; j++){
+
+    public static void mostrarMatrizSemana(int[][] semana) {
+        for (int i = 0; i < semana.length; i++) {
+            for (int j = 0; j < semana[0].length; j++) {
                 System.out.print(semana[i][j] + " ");
             }
             System.out.println();
         }
     }
-    public static int[][] llenarMatrizSemana(int [][] semana){
-        for(int x = 0; x < semana.length; x++){
-            for(int y = 0; y < semana[0].length; y++){
-                semana[x][y] = (int) ((Math.random() * 10) + 1);
+
+    public static int[][] llenarMatrizSemana(int[][] semana) {
+        for (int x = 0; x < semana.length; x++) {
+            for (int y = 0; y < semana[0].length; y++) {
+                semana[x][y] = (int) ((Math.random() * 30) + 1);
             }
         }
         return semana;
-    }
-    public static int[] calcularSumaDeFilas(int[][] semana){
-        int[] sumas = new int[semana.length]; // Array para almacenar las sumas de las filas
-
-        for (int fila = 0; fila < semana.length; fila++) {
-            int suma = 0; // Inicializa la suma para cada fila
-            for (int columna = 0; columna < semana[0].length; columna++) {
-                suma += semana[fila][columna];
-            }
-            System.out.println("La suma de la fila " + fila + " es: " + suma);
-            sumas[fila] = suma; // Almacena la suma en el array
-        }
-
-        return sumas;
     }
 
     public static double[] calcularPromedioDeFilas(int[][] semana) {
@@ -62,19 +57,35 @@ public class Main {
         return promedios;
     }
 
-    public static int obtenerDiaMasFrio() {
+    public static double obtenerDiaMasFrio(double[] promedios) {
+        double diaFrio = promedios[0];
+
+        for (int i = 1; i < promedios.length; i++) {
+            if (promedios[i] < diaFrio) {
+                diaFrio = promedios[i];
+            }
+        }
+
+        return diaFrio;
+    }
+
+    public static double obtenerDiaMasCaluroso(double[] promedios) {
+        double diaCaluroso = promedios[0];
+
+        for (int i = 1; i < promedios.length; i++) {
+            if (promedios[i] > diaCaluroso) {
+                diaCaluroso = promedios[i];
+            }
+        }
+
+        return diaCaluroso;
+    }
+
+    public static int obtenerHoraMasCalurosa() {
         return 0;
     }
 
-    public static int obtenerDiaMasCaluroso(){
-        return 0;
-    }
-
-    public static int obtenerHoraMasCalurosa(){
-        return 0;
-    }
-
-    public static int obtenerHoraMasFria(){
+    public static int obtenerHoraMasFria() {
         return 0;
     }
 }
